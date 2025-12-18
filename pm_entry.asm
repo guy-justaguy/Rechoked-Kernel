200992c7f7656fd45670396cb2785f95f19d071f
guy-justaguy <aydin.nawaf@gmail.com>
2025-12-18 22:05:51 +0600

    Update pm_entry.asm

--- pm_entry.asm
+++ pm_entry.asm
@@ -1,4 +1,18 @@
-[BITS 32]
+[BITS 32] 
+
+--- MULTIBOOT HEADER (The GRUB Handshake) ---
+MBOOT_PAGE_ALIGN    equ 1 << 0
+MBOOT_MEM_INFO      equ 1 << 1
+MBOOT_HEADER_MAGIC  equ 0x1BADB002
+MBOOT_HEADER_FLAGS  equ MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
+MBOOT_CHECKSUM      equ -(MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+
+section .multiboot
+    align 4
+    dd MBOOT_HEADER_MAGIC
+    dd MBOOT_HEADER_FLAGS
+    dd MBOOT_CHECKSUM
+
 protected_mode_start:
     ; Load the 32-bit data selector (0x10 is often the offset for the Data Segment in GDT)
     mov ax, 0x10
@@ -36,4 +50,5 @@ align 16
 stack_bottom:
     resb 16384        ; 16KB of raw stack space
 stack_top:
+
 
